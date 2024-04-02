82cf7ed92b8dc4a81f8a5b9afb1409cb5bb62535
ThaSMorato <tha.s.morato@gmail.com>
2024-04-02 01:29:46 -0300

    Feat: initial pages

---
+++ src/components/input.tsx
@@ -0,0 +1,29 @@
+import { colors } from "@/styles/colors"
+import { ReactNode } from "react"
+import { TextInput, View, TextInputProps } from "react-native"
+
+interface InputProps {
+  children: ReactNode
+}
+
+const Input = ({children}: InputProps) => {
+  return (
+    <View className="w-full h-14 items-center flex-row gap-3 p-3 border border-green-400 rounded-lg">
+      { children }
+    </View>
+  )
+}
+
+const Field = (props: TextInputProps) => {
+  return (
+    <TextInput
+      className="flex-1 text-white text-base font-regular"
+      placeholderTextColor={colors.gray[200]}
+      {...props}
+    />
+  )
+}
+
+Input.Field = Field
+
+export { Input }
